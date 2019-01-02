3e2fe65655e40cf6133eb3eeea011925f553c2e9
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-02 14:09:29 -0600

    Refactored Landing Page using react-bootstrap
    Refactored navbar and landing page using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Navigation, Drawer, Content} from 'react-mdl';
+import { Layout, Content} from 'react-mdl';
 import Main from './Components/Main';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 
 
@@ -13,41 +13,26 @@ class App extends Component {
       <div>
           <Layout>
             <header>
-            <Navbar inverse collapseOnSelect>
+            <Navbar inverse collapseOnSelect fixedTop>
             <Navbar.Header>
               <Navbar.Brand>
-                <a href="#brand">React-Bootstrap</a>
+                <Link to="/">Allen Justin Blanco</Link>
               </Navbar.Brand>
               <Navbar.Toggle />
             </Navbar.Header>
               <Navbar.Collapse>
-                <Nav>
-                  <NavItem eventKey={1} href="#">
+                <Nav pullRight>
+                  <NavItem eventKey={1}>
                     <Link to="/Resume">Resume</Link>
                   </NavItem>
-                  <NavItem eventKey={2} href="#">
+                  <NavItem eventKey={2}>
                      <Link to="/AboutMe">About me</Link>
                   </NavItem>
-                  <NavItem eventKey={3} href="#">
+                  <NavItem eventKey={3}>
                     <Link to="/Projects">Projects</Link>
                   </NavItem>
-                  <NavItem eventKey={4} href="#">
+                  <NavItem eventKey={4}>
                     <Link to="/Contact">Contact</Link>
-                  </NavItem>
-                  <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Action</MenuItem>
-                    <MenuItem eventKey={3.2}>Another action</MenuItem>
-                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                  </NavDropdown>
-                </Nav>
-                <Nav pullRight>
-                  <NavItem eventKey={1} href="#">
-                    Link
-                  </NavItem>
-                  <NavItem eventKey={2} href="#">
-                    Link Right
                   </NavItem>
                 </Nav>
               </Navbar.Collapse>
--- src/Components/Landingpage.js
+++ src/Components/Landingpage.js
@@ -1,36 +1,22 @@
 import React, { Component } from 'react';
-import { Grid, Cell} from 'react-mdl';
+import { Jumbotron } from 'react-bootstrap'
 
 
 class Landingpage extends Component {
   render() {
     return (
-      <div style={{width: '100%', margin: 'auto'}}>
-        <Grid className="Landing-grid">
-          <Cell col={12}>
-            <div>
-              <h2>Hello, I'm Allen Justin Blanco,</h2>
-              <h2>I'm a Full Stack Web Developer</h2>
-              <p>| HTML | CSS | Bootstrap | JavaScript | Jquery | React |</p>
-              <div className='social-links'>
-                <a href='https://www.linkedin.com/in/allen-justin-blanco-393a28176/' target='_blank' rel='noopener noreferrer'>
-                  <i class="fab fa-linkedin"></i>
-                </a>
-                <a href='https://github.com/allenjustinblanco' target='_blank' rel='noopener noreferrer'>
-                  <i class="fab fa-github"></i>
-                </a>
-              </div>
-            </div>
-          </Cell>
-          <div style={{width: '80%', margin: 'auto'}}>
-            <Grid>
-                <Cell col={4}></Cell>
-                <Cell col={4}>4</Cell>
-                <Cell col={4}></Cell>
-            </Grid>
-            </div>
-        </Grid>
-      </div>
+      <Jumbotron >
+        <h3 className='Quicksand'>Full Stack Developer</h3>
+        <p className='cursive'>“There are three responses to a piece of design – yes, no, and WOW! Wow is the one to aim for." - Milton Glaser</p>
+        <div className='social-links'>
+          <a href='https://www.linkedin.com/in/allen-justin-blanco-393a28176/' target='_blank' rel='noopener noreferrer'>
+            <i class="fab fa-linkedin"></i>
+          </a>
+          <a href='https://github.com/allenjustinblanco' target='_blank' rel='noopener noreferrer'>
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+        </Jumbotron>
     )
   }
 }
