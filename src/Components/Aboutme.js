ce052bd396f2c222d19d5049b2334bf420419c75
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-03 09:01:48 -0600

    Refactored Aboutme page

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { CSSTransitionGroup } from 'react-transition-group'
+import '../styles/AboutMeStyle.css'
 
-class Aboutme extends Component {
-  render() {
+const AboutMe = () => {
     return (
-      <div>
-        <h1>Allen Justin Blanco</h1>
-        <p>The one thing I value the most is time. So I try to make every second count!</p>
-      </div>
+      <CSSTransitionGroup
+        transitionName="aboutMeTransition"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}>
+          <div>
+            <h1>AboutMe Page</h1>
+          </div>
+      </CSSTransitionGroup>
     )
-  }
 }
 
-export default Aboutme;
+export default AboutMe;
