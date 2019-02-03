215f13d23287f3678e2cbbe84966d9166a0e97c0
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-03 09:09:12 -0600

    Refactored App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Layout, Content} from 'react-mdl';
-import Main from './Components/Main';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import Menu from './Components/Menu'
+import Main from './Components/Main'
+import './styles/app.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-          <Layout>
-            <header>
-            <Navbar inverse collapseOnSelect fixedTop>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <Link to="/">AJB</Link>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-              <Navbar.Collapse>
-                <Nav pullRight>
-                  <NavItem eventKey={1}>
-                    <Link to="/Resume">Resume</Link>
-                  </NavItem>
-                  <NavItem eventKey={2}>
-                     <Link to="/AboutMe">About me</Link>
-                  </NavItem>
-                  <NavItem eventKey={3}>
-                    <Link to="/Projects">Projects</Link>
-                  </NavItem>
-                  <NavItem eventKey={4}>
-                    <Link to="/Contact">Contact</Link>
-                  </NavItem>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>;
-            </header>
-            <Content>
-              <Main />
-            </Content>
-          </Layout>
+        <Menu />
+        <Main />
       </div>
     );
   }
