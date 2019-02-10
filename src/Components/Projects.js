f4c31ebc8599945caf0b598d2397fb1f91ae9d34
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-10 13:40:10 -0600

    add image to cards

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { CSSTransitionGroup } from 'react-transition-group'
 import '../styles/ProjectsStyle.css'
+import architecture from '../architecture.jpg'
 
 const imageStyle = {
-    width: '100%'
+    width: '100%',
+    height: '100px'
 };
 
 const Projects = () => {
@@ -14,12 +16,42 @@ const Projects = () => {
         transitionAppearTimeout={500}
         transitionEnter={false}
         transitionLeave={false}>
+        <div className="cards">
         <div className="card">
-        <img src={"../architecture.png"} alt="Avatar" style={imageStyle}/>
+        <img src={architecture} alt="Avatar" style={imageStyle}/>
+          <div class="container">
+            <h4><b>Movie Application</b></h4>
+            <p>HTML5, CSS3, Javascript, Jquery, Ajax</p>
+          </div>
+        </div>
+        <div className="card">
+        <img src={architecture} alt="Avatar" style={imageStyle}/>
+          <div class="container">
+            <h4><b>Coffee Project</b></h4>
+            <p>HTML5, CSS3, Javascript, Jquery</p>
+          </div>
+        </div>
+        <div className="card">
+        <img src={architecture} alt="Avatar" style={imageStyle}/>
           <div class="container">
             <h4><b>John Doe</b></h4>
             <p>Architect & Engineer</p>
           </div>
+        </div>
+        <div className="card">
+        <img src={architecture} alt="Avatar" style={imageStyle}/>
+          <div class="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+        <div className="card">
+        <img src={architecture} alt="Avatar" style={imageStyle}/>
+          <div class="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
         </div>
       </CSSTransitionGroup>
     )
